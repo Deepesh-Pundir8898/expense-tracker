cc3f08f995142bd9b822b599054c881cff8a4583
Deepesh-Pundir <deepeshpundir@gmail.com>
2025-04-23 21:28:44 +0530

    solve the build error

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
-      ? "https://expense-tracker-1-9j3j.onrender.com/api/v1" // Production URL
+      ? "https://expense-tracker-1-9j3j.onrender.com/api/v1"
       : "http://localhost:8080/api/v1",
 });
 
