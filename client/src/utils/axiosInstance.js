ea92d60f553e1bc915e646fda15dd84e4aed2fe7
Deepesh-Pundir <deepeshpundir@gmail.com>
2025-04-23 21:47:40 +0530

    add baseurl for production

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,7 +1,10 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "http://localhost:8080/api/v1",
+  baseURL:
+    process.env.NODE_ENV === "production"
+      ? "https://expense-tracker-1-9j3j.onrender.com/api/v1" // Production URL
+      : "http://localhost:8080/api/v1", // Development URL
 });
 
 export default axiosInstance;
