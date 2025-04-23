fdedf527e4269037984ea65bd1e5e3f2d3ce49fc
Deepesh-Pundir <deepeshpundir@gmail.com>
2025-04-23 21:57:06 +0530

    rollback the changes

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -1,10 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL:
-    process.env.NODE_ENV === "production"
-      ? "https://expense-tracker-1-9j3j.onrender.com/api/v1" // Production URL
-      : "http://localhost:8080/api/v1", // Development URL
+  baseURL: "http://localhost:8080/api/v1",
 });
 
 export default axiosInstance;
