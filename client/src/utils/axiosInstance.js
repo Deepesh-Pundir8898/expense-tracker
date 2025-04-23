6a6ca769df36caba079b76c1dc2e4cdac82833be
Deepesh-Pundir <deepeshpundir@gmail.com>
2025-04-23 21:06:08 +0530

    rebuild

--- client/src/utils/axiosInstance.js
+++ client/src/utils/axiosInstance.js
@@ -4,7 +4,7 @@ const axiosInstance = axios.create({
   baseURL:
     process.env.NODE_ENV === "production"
       ? "https://expense-tracker-1-9j3j.onrender.com/api/v1" // Production URL
-      : "http://localhost:8080/api/v1", // Development URL
+      : "http://localhost:8080/api/v1",
 });
 
 export default axiosInstance;
